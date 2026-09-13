be588dd8237f111ec8b687981fde8e667b8eaaa0
Raghav Sharma <raghavv.s2003@gmail.com>
2026-09-13 09:30:03 +0530

    fix: use H2 in-memory database for tests, add test profile

--- src/test/java/com/grievance/grievance_tracker/GrievanceTrackerApplicationTests.java
+++ src/test/java/com/grievance/grievance_tracker/GrievanceTrackerApplicationTests.java
@@ -2,12 +2,14 @@ package com.grievance.grievance_tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class GrievanceTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
